2190535a2f1eb413ef7a9f6f066a72cdf6cd1c64
Nitish Katkade <nitishkatkade94@gmail.com>
2025-06-28 15:31:43 +0530

    endpoint added

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from pathlib import Path
 
-app = FastAPI()
+from fastapi import FastAPI, UploadFile
+from fastapi.exceptions import HTTPException
+from fastapi.responses import JSONResponse
+from langchain_core.documents.base import Document
+from langchain_core.prompts.chat import ChatPromptTemplate
+
+from core.docs_extractor import get_extracted_document
+from core.llm_extractor import extract_structured_information
+from core.inference import get_chat_inference
+from core.prompt import get_prompt_template
+from core.utils import is_allowed_file, save_file
+
+app: FastAPI = FastAPI()
 
 
 @app.get("/")
-async def get_server_status():
+async def get_server_status() -> JSONResponse:
     return JSONResponse(content={"message": "live!"})
+
+
+@app.post("/resume_uploader")
+async def upload_resume(file: UploadFile) -> JSONResponse:
+    if is_allowed_file(file.filename) is False:
+        raise HTTPException(status_code=400, detail="Unsupported file type.")
+
+    file_path: Path = save_file(file)
+    docs: list[Document] = await get_extracted_document(file_path)
+    # print(docs)
+
+    prompt: ChatPromptTemplate = get_prompt_template()
+
+    llm = get_chat_inference()
+
+    result = extract_structured_information(llm, prompt, docs)
+
+    return result
